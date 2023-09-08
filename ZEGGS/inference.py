79dcf729d90fc8506ee402a3ad71d1c8f1b05779
jh-gglabs <junhwan@goodganglabs.com>
2023-09-08 13:54:05 +0900

    Add style emotion argument

--- ZEGGS/inference.py
+++ ZEGGS/inference.py
@@ -34,12 +34,18 @@ class ZeroEggsInference(object):
         # Load configs
         self.stat_data, self.data_pipeline_conf, self.details = self._load_data_configs(config_directory_path)
 
-    def generate_gesture_from_audio_file(self, audio_file_path, style_bvh_path, style_encoding_type="example"):
+    def generate_gesture_from_audio_file(self, audio_file_path, style_bvh_path, style_encoding_type="example", style_name=None):
         """
         Args:
             audio_file_path: Path to audio file.
             style_bvh_path: Path to style bvh file.
-            style_encoding_type: How to encode the style. Either "example" or "label". Defaults to "example".
+            style_encoding_type: How to encode the style. Either "example" or "label". Defaults to "example". If "label" type, specify the style_name argument.
+            style_name: Style name. Style categories covers 19 different motion styles and can be selected from the following.
+                "Agreement", "Angry", "Disagreement", "Distracted", "Flirty", "Happy", "Laughing",
+                "Oration", "Neutral", "Old", "Pensive", "Relaxed", "Sad", "Sarcastic", "Scared",
+                "Sneaky", "Threatening", "Tired"
+        Returns:
+            bvh_output: Body Gestures Output in BVH format
         """
         with torch.no_grad():
             _, audio_data = read_wavfile(
@@ -66,7 +72,12 @@ class ZeroEggsInference(object):
             speech_encoding = self.speech_encoder_network(audio_features)
 
             # 2) Style Encoding
-            style_encoding, animation_data = self._create_style_encoding(style_bvh_path)
+            style_encoding, animation_data = self._create_style_encoding(
+                style_bvh_path=style_bvh_path,
+                style_encoding_type=style_encoding_type,
+                style_name=style_name,
+                temperature=1.0
+            )
 
             # 3) Decode and Generate Gestures
             bvh_output = self._make_bvh_outputs(
@@ -76,12 +87,18 @@ class ZeroEggsInference(object):
             )
             return bvh_output
 
-    def generate_gesture_from_audio_chunk(self, audio_chunk, style_bvh_path, style_encoding_type="example"):
+    def generate_gesture_from_audio_chunk(self, audio_chunk, style_bvh_path, style_encoding_type="example", style_name=None):
         """
         Args:
             audio_chunk: Numpy array of audio chunk data
             style_bvh_path: Path to style bvh file.
-            style_encoding_type: How to encode the style. Either "example" or "label". Defaults to "example".
+            style_encoding_type: How to encode the style. Either "example" or "label". Defaults to "example". If "label" type, specify the style_name argument.
+            style_name: Style name. Style categories covers 19 different motion styles and can be selected from the following.
+                "Agreement", "Angry", "Disagreement", "Distracted", "Flirty", "Happy", "Laughing",
+                "Oration", "Neutral", "Old", "Pensive", "Relaxed", "Sad", "Sarcastic", "Scared",
+                "Sneaky", "Threatening", "Tired"
+        Returns:
+            bvh_output: Body Gestures Output in BVH format
         """
         n_frames = int(round(60.0 * (len(audio_chunk) / 16000)))
 
@@ -104,7 +121,12 @@ class ZeroEggsInference(object):
             speech_encoding = self.speech_encoder_network(audio_features)
 
             # 2) Style Encoding
-            style_encoding, animation_data = self._create_style_encoding(style_bvh_path)
+            style_encoding, animation_data = self._create_style_encoding(
+                style_bvh_path=style_bvh_path,
+                style_encoding_type=style_encoding_type,
+                style_name=style_name,
+                temperature=1.0
+            )
 
             # 3) Decode and Generate Gestures
             bvh_output = self._make_bvh_outputs(
@@ -181,44 +203,44 @@ class ZeroEggsInference(object):
         )
         return bvh_info
 
-    def _create_style_encoding(self, style_bvh_path, style_encoding_type="example", temperature=1.0):
+    def _create_style_encoding(self, style_bvh_path, style_encoding_type="example", temperature=1.0, style_name=None):
+        anim_data = bvh.load(style_bvh_path)
+        anim_fps = int(np.ceil(1 / anim_data["frametime"]))
+        assert anim_fps == 60
+
+        # Extracting features
+        (
+            root_pos,
+            root_rot,
+            root_vel,
+            root_vrt,
+            lpos,
+            lrot,
+            ltxy,
+            lvel,
+            lvrt,
+            cpos,
+            crot,
+            ctxy,
+            cvel,
+            cvrt,
+            gaze_pos,
+            gaze_dir,
+        ) = preprocess_animation(anim_data)
+
+         # convert to tensor
+        nframes = len(anim_data["rotations"])
+        root_vel = torch.as_tensor(root_vel, dtype=torch.float32, device=self.device)
+        root_vrt = torch.as_tensor(root_vrt, dtype=torch.float32, device=self.device)
+        root_pos = torch.as_tensor(root_pos, dtype=torch.float32, device=self.device)
+        root_rot = torch.as_tensor(root_rot, dtype=torch.float32, device=self.device)
+        lpos = torch.as_tensor(lpos, dtype=torch.float32, device=self.device)
+        ltxy = torch.as_tensor(ltxy, dtype=torch.float32, device=self.device)
+        lvel = torch.as_tensor(lvel, dtype=torch.float32, device=self.device)
+        lvrt = torch.as_tensor(lvrt, dtype=torch.float32, device=self.device)
+        gaze_pos = torch.as_tensor(gaze_pos, dtype=torch.float32, device=self.device)
+
         if style_encoding_type == "example":
-            anim_data = bvh.load(style_bvh_path)
-            anim_fps = int(np.ceil(1 / anim_data["frametime"]))
-            assert anim_fps == 60
-
-            # Extracting features
-            (
-                root_pos,
-                root_rot,
-                root_vel,
-                root_vrt,
-                lpos,
-                lrot,
-                ltxy,
-                lvel,
-                lvrt,
-                cpos,
-                crot,
-                ctxy,
-                cvel,
-                cvrt,
-                gaze_pos,
-                gaze_dir,
-            ) = preprocess_animation(anim_data)
-
-            # convert to tensor
-            nframes = len(anim_data["rotations"])
-            root_vel = torch.as_tensor(root_vel, dtype=torch.float32, device=self.device)
-            root_vrt = torch.as_tensor(root_vrt, dtype=torch.float32, device=self.device)
-            root_pos = torch.as_tensor(root_pos, dtype=torch.float32, device=self.device)
-            root_rot = torch.as_tensor(root_rot, dtype=torch.float32, device=self.device)
-            lpos = torch.as_tensor(lpos, dtype=torch.float32, device=self.device)
-            ltxy = torch.as_tensor(ltxy, dtype=torch.float32, device=self.device)
-            lvel = torch.as_tensor(lvel, dtype=torch.float32, device=self.device)
-            lvrt = torch.as_tensor(lvrt, dtype=torch.float32, device=self.device)
-            gaze_pos = torch.as_tensor(gaze_pos, dtype=torch.float32, device=self.device)
-
             S_root_vel = root_vel.reshape(nframes, -1)
             S_root_vrt = root_vrt.reshape(nframes, -1)
             S_lpos = lpos.reshape(nframes, -1)
@@ -244,13 +266,19 @@ class ZeroEggsInference(object):
             style_encoding, _, _ = self.style_encoder_network(
                 example_feature_vec[np.newaxis], temperature
             )
+        
+        elif (style_encoding_type == "label") & (style_name is not None):
+            n_labels = len(self.details["label_names"])
+            style_index = self.details["label_names"].index(style_name)
+            style_encoding = torch.zeros((1, n_labels), dtype=torch.float32, device=self.device)
+            style_encoding[0, style_index] = 1.0
 
-            animation_data = (
-                root_pos, root_rot, root_vel, root_vrt,
-                lpos, ltxy, lvel, lvrt, gaze_pos
-            )
+        animation_data = (
+            root_pos, root_rot, root_vel, root_vrt,
+            lpos, ltxy, lvel, lvrt, gaze_pos
+        )
 
-            return style_encoding, animation_data
+        return style_encoding, animation_data
 
     def _load_data_configs(self, config_dir):
         # Load configs
@@ -296,9 +324,18 @@ if __name__ == "__main__":
         config_directory_path=config_dir
     )
 
+    # Style Encoding Type
+    ## "Example" based
     audio_file_path = "../data/clean/067_Speech_2_x_1_0.wav"
     bvh_file_path = "../data/clean/067_Speech_2_x_1_0.bvh"
 
-    bvh_output = zeggs_module.generate_gesture_from_audio_file(audio_file_path, bvh_file_path)
-    bvh.save("./test.bvh", bvh_output)
+    bvh_output = zeggs_module.generate_gesture_from_audio_file(
+        audio_file_path, bvh_file_path, style_encoding_type="example"
+    )
 
+    ## "Label" based
+    bvh_output = zeggs_module.generate_gesture_from_audio_file(
+        audio_file_path, bvh_file_path, style_encoding_type="label", style_name="Sad"
+    )
+
+    # bvh.save("./test.bvh", bvh_output)
